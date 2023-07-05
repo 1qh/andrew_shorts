362a7697239f52bc94f6c4da10c04b5e238103ca
Huy <laiquanghuy24122001@gmail.com>
2023-07-05 18:42:46 +0700

    .

---
+++ lc/a.py
@@ -0,0 +1,3 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
