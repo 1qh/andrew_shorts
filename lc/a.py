f936ccf6782941e133bedfd9aba66f5016e819b6
Huy <laiquanghuy24122001@gmail.com>
2023-07-11 18:33:41 +0700

    .

--- lc/a.py
+++ lc/a.py
@@ -1,12 +1,20 @@
+import langchain
+from langchain.agents import AgentType, initialize_agent, load_tools, tool
+from langchain.agents.agent_toolkits import create_python_agent
 from langchain.chains import (
     ConversationChain,
     LLMChain,
+    RetrievalQA,
     SequentialChain,
     SimpleSequentialChain,
 )
 from langchain.chains.router import MultiPromptChain
 from langchain.chains.router.llm_router import LLMRouterChain, RouterOutputParser
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.evaluation.qa import QAEvalChain, QAGenerateChain
+from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.memory import (
     ConversationBufferMemory,
@@ -16,3 +24,8 @@ from langchain.memory import (
 )
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.python import PythonREPL
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.vectorstores import DocArrayInMemorySearch
+
+langchain.debug = True
