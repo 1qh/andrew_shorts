9dd25764b100006ba0c5a9530b0e773dce4da12a
Huy <laiquanghuy24122001@gmail.com>
2023-07-13 19:25:50 +0700

    .

--- lc/a.py
+++ lc/a.py
@@ -2,17 +2,29 @@ import langchain
 from langchain.agents import AgentType, initialize_agent, load_tools, tool
 from langchain.agents.agent_toolkits import create_python_agent
 from langchain.chains import (
+    ConversationalRetrievalChain,
     ConversationChain,
     LLMChain,
     RetrievalQA,
     SequentialChain,
     SimpleSequentialChain,
 )
+from langchain.chains.query_constructor.base import AttributeInfo
 from langchain.chains.router import MultiPromptChain
 from langchain.chains.router.llm_router import LLMRouterChain, RouterOutputParser
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import CSVLoader
+from langchain.document_loaders import (
+    CSVLoader,
+    NotionDirectoryLoader,
+    PyPDFLoader,
+    TextLoader,
+    WebBaseLoader,
+)
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers.audio import OpenAIWhisperParser
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.evaluation.qa import QAEvalChain, QAGenerateChain
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
@@ -25,7 +37,20 @@ from langchain.memory import (
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.python import PythonREPL
+from langchain.retrievers import (
+    ContextualCompressionRetriever,
+    SVMRetriever,
+    TFIDFRetriever,
+)
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    MarkdownHeaderTextSplitter,
+    RecursiveCharacterTextSplitter,
+    TokenTextSplitter,
+)
 from langchain.tools.python.tool import PythonREPLTool
-from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.vectorstores import Chroma, DocArrayInMemorySearch
 
 langchain.debug = True
